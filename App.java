437ca337ae27931eb80448de894664dc952ee571
ManelPizarro <132475276+ManelPizarro@users.noreply.github.com>
2023-05-05 12:32:29 +0200

    Add files via upload
    Ejercicio 4

--- App.java
+++ App.java
@@ -1,39 +1,69 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //parte 1
+       
+       //declaración de variables
+        double numeroif = 3;
+        String resultadoIf = "Numeroif";
+        int NumeroWhile = 0;
+        String Estacion = "Verano";             
         
-        int suma;
-        suma = Sumame (1,5,8);
-        System.out.println("suma de 1,5 y 8 ");
-        System.out.println(suma);
-        //parte 2
+        System.out.println("BLOQUE IF");
+        //bloque IF
+        if (numeroif < 0) {
+            resultadoIf = resultadoIf + " es menor que 0";
 
-        coche micoche = new coche();
-        micoche.añadirpuerta();
-        System.out.println("numero de puertas del vehículo");
-        System.out.println(micoche.Puertas);
-    }
-
-    public static int Sumame(int a, int b, int c) {
-        int resultado;
-        resultado =a+b+c;
-        return resultado;
-        
-    }
-
-    public static void sumapuertas() {
-
-    }
-    public static class coche{
-        
-        String Marca;
-        String Modelo;
-        int Puertas = 0;
-        int cubicaje;
-        String Combustible;
-        public void añadirpuerta() {
-            this.Puertas++;
+        } else if (numeroif > 0) {
+            resultadoIf = resultadoIf + " es mayor que 0";
+        } else {
+            resultadoIf = resultadoIf + " es 0";
         }
-    }
+        System.out.println(resultadoIf);
+        System.out.println("");
+        
+        //bloque While
+        System.out.println("BLOQUE WHILE");
+        while (NumeroWhile < 3) {
+            NumeroWhile++;
+            System.out.println(NumeroWhile);
 
+        }
+        System.out.println("");
+        System.out.println("BLOQUE DO - WHILE");
+
+        //bloque do
+
+        do  {
+            NumeroWhile++;
+            System.out.println(NumeroWhile);
+        } while (NumeroWhile < 4);
+
+        System.out.println("");
+        System.out.println("BLOQUE FOR");
+        //bloque for
+        for (int NumeroFor = 0;NumeroFor < 3;NumeroFor++) {
+            System.out.println(NumeroFor);
+        }
+
+        System.out.println("");
+        System.out.println("BLOQUE SWITCH");
+        //Bloque Switch
+        switch (Estacion) {
+            case "Primavera":
+                System.out.println("Es Primavera");
+                break;
+            case "Verano":
+                System.out.println("Es Verano");
+                break;
+            case "Otoño":
+                System.out.println("Es Otoño");
+                break;
+            case "Invierno":
+                System.out.println("Es Invierno");
+                break;
+            default:
+                System.out.println("No es una estación del año");
+
+        }
+
+    }
 }
